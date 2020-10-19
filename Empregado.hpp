7d33ad3f3d1db013ce5f802e02c6a2327c8329d5
GabrielNoujeimi <73117115+GabrielNoujeimi@users.noreply.github.com>
2020-10-19 16:02:31 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -1,27 +1,14 @@
 #ifndef EMPREGADO_H
 #define EMPREGADO_H
 
-#include <iostream>
-#include <string>
-
 class Empregado {
 	
+  private:
+    double salarioHora;
+      
   public:
-    double salarioHora;  
-    double quotaMensalVendas;  
-
-
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
+    double pagamentoMes(double horasTrabalhadas);
+    void setSalarioHora(double salarioHora);
 	
 };
 
