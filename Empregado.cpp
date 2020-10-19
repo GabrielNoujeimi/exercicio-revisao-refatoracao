d43a5be122a06cf87196e1666c896788d57e4462
GabrielNoujeimi <73117115+GabrielNoujeimi@users.noreply.github.com>
2020-10-19 16:01:43 -0300

    Create Empregado.cpp

---
+++ Empregado.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+# include "Empregado.hpp"
+# define TrabalhoDiario 8
+
+double Empregado::pagamentoMes(double horasTrabalhadas) {
+ 
+	  
+	//Cálculo de hora extra (+50% se horasTrabalhadas > 8)
+    if (horasTrabalhadas > 8) {
+     double horaExtra = horasTrabalhadas - TrabalhoDiario;
+     horasTrabalhadas += horaExtra / 2;
+     }
+	return horasTrabalhadas * salarioHora;
+}
+
+void Empregado::setSalarioHora(double salarioHora){
+    this->salarioHora =  salarioHora;
+}
